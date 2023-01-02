a9ffe4d2e424fd060b3ab8ba48aa1adec7f79bd0
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-01-02 16:49:38 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -29,7 +29,6 @@ export const App = () => {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={statistics} />
-      <Statistics stats={statistics} />
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />
     </div>
